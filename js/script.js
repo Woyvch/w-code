2ad421bd993efe744f412d37478f62541dfeb2ec
Wouter Scherpereel <woyvch@gmail.com>
2022-01-30 15:44:04 +0100

    Adding modals to the project section

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
 /* jshint esversion: 6 */
+
+// Get the modal
+//var modal = document.getElementById("modal");
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    //modal.style.display = "none";
+    document.getElementsByClassName('modal').style.display = 'none';
+  }
+};
+
+// Open the Modal
+function openModal(modal) {
+  document.getElementById(`${modal}`).style.display = "block";
+}
+
+// Close the Modal
+function closeModal(modal) {
+  document.getElementById(`${modal}`).style.display = 'none';
+}
